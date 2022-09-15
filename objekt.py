c478bb283959c59d2caf4042446a087bce0606ed
maxeliwax <max.walldov@gmail.com>
2022-09-15 12:50:45 +0200

    added class

---
+++ objekt.py
@@ -0,0 +1,7 @@
+class Vermdoleden:
+    def __init__(self, korfällt, rastplatser, avfarter, hastighet, bilar ):
+        self.korfällt = korfällt
+        self.rastplater = rastplatser
+        self.avfarter = avfarter
+        self. hastighet = hastighet
+        self.bilar = bilar 
